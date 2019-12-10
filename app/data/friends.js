dfb92de2523f0b15c4da0b4f5179c162d48487fd
cvevera <cvevera@gmail.com>
2019-12-10 01:50:28 -0500

    built out functionality for the match logic and added a modal to the survey.html (not working)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,23 @@
 var friends = [
     {
         name: "Corey",
-        photo: "img",
-        scores: [1,2,3,4,5,4,3,2,1]
+        photo: "https://unsplash.com/photos/J1OScm_uHUQ",
+        scores: [1,2,3,4,5,4]
+    },
+    {
+        name: "Julie",
+        photo: "https://unsplash.com/photos/J1OScm_uHUQ",
+        scores: [5,4,2,3,1,5]
+    },
+    {
+        name: "Pete",
+        photo: "https://unsplash.com/photos/J1OScm_uHUQ",
+        scores: [5,3,4,2,1,5]
+    },
+    {
+        name: "Rodger",
+        photo: "https://images.unsplash.com/photo-1543949806-2c9935e6aa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
+        scores: [5,5,5,5,5,5]
     }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,40 @@ app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
-  app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body);
-      res.json(true);
-  })
-};
+app.post("/api/friends", function(req, res) {
+
+        var match = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+
+        var userinputs = req.body
+        var userScores = userinputs.scores
+        console.log("user Scores" + userScores)
+        var totalDifference = 0;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            console.log(friendsData[i])
+          totalDifference = 0;
+
+            for (var j = 0; j < friendsData[i].scores[j]; j++){
+
+                totalDifference == Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+                console.log(Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j])));
+                if (totalDifference <= match.difference) {
+
+                    match.name = friendsData[i].name;
+                    match.photo = friendsData[i].photo;
+                    match.difference = totalDifference;
+
+                }
+            }
+        }
+
+
+
+      friendsData.push(userinputs);
+      console.log(match)
+      res.json(match);
+  })};
